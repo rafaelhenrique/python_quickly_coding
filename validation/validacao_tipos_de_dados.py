6e13607ff4dab6decfbbd4ead281e343803948e0
Rafael Henrique da Silva Correia <rsilva@komputer.com.br>
2015-03-12 14:37:34 -0300

    Good practice of validation

---
+++ validation/validacao_tipos_de_dados.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#import types
+s = ''
+# if isinstance(s, types.StringType):
+#     print('s é uma string.')
+
+if isinstance(s, str):
+    print('s is string!')
