554c6870627c72da11a55ae6a2459e252e279139
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-06-25 20:57:38 -0300

    Basic example for create a module

---
+++ modules/main.py
@@ -0,0 +1,7 @@
+import module_test
+from module_test import hello
+
+if __name__ == "__main__":
+    module_test.hello("Rafael")
+    # this hello is diferent for the other hello ;)
+    hello()
---
+++ modules/module_test/__init__.py
@@ -0,0 +1,3 @@
+
+def hello(name="Nobody"):
+    print(name)
