95c428bb5f4fd8ceffca774f5866347a2f45bb69
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-06-25 21:23:54 -0300

    say.hello() worked \o/

--- modules/main.py
+++ modules/main.py
@@ -1,8 +1,11 @@
 import module_test
 from module_test import hello
+from module_test import say
 
 if __name__ == "__main__":
     module_test.hello("Rafael")
     # this hello is diferent for the other hello ;)
     # or not??
     hello()
+    # and now??
+    say.hello()
